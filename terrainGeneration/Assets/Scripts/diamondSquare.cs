2a8bb1c2f74fd5c4afb930957f430522575f7553
Rishabh Sharma <rishsharma13@gmail.com>
2016-09-04 19:03:36 +1000

    added terrain object

---
+++ terrainGeneration/Assets/Scripts/diamondSquare.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diamondSquare : MonoBehaviour {
+
+	Terrain currentTerrain;
+	TerrainData tData;
+	Vector3 terrainSize;
+	// Use this for initialization
+	void Start () {
+
+		currentTerrain = Terrain.activeTerrain;
+		tData = currentTerrain.terrainData;
+		terrainSize = tData.size;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (terrainSize);
+
+	}
+}
